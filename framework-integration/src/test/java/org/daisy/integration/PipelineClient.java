895e4ae2eb9af41c04950e74b7eb795d9cf0196e
capitancambio <capitan.cambio@gmail.com>
2014-08-21 17:53:17 +0100

    Add authentication to the client

--- framework-integration/src/test/java/org/daisy/integration/PipelineClient.java
+++ framework-integration/src/test/java/org/daisy/integration/PipelineClient.java
@@ -1,27 +1,48 @@
 package org.daisy.integration;
 
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientRequestFilter;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.UriBuilder;
 
+import org.apache.commons.codec.binary.Base64;
 import org.daisy.pipeline.webservice.jabx.base.Alive;
+import org.daisy.pipeline.webservice.jabx.client.Client;
+import org.daisy.pipeline.webservice.jabx.client.Clients;
 import org.daisy.pipeline.webservice.jabx.job.Job;
 import org.daisy.pipeline.webservice.jabx.job.Jobs;
 import org.daisy.pipeline.webservice.jabx.queue.Queue;
 import org.daisy.pipeline.webservice.jabx.request.JobRequest;
 import org.daisy.pipeline.webservice.jabx.script.Scripts;
-import org.daisy.pipeline.webservice.jabx.client.Clients;
-import org.daisy.pipeline.webservice.jabx.client.Client;
+import org.glassfish.jersey.client.ClientConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Strings;
 /**
  * Simple but full-featured pipline2 WS client
  */
 public class PipelineClient {
 
+        private static final Logger logger = LoggerFactory.getLogger(PipelineClient.class);
+
         private WebTarget target;
 
         public PipelineClient(String baseUri) {
                 this.target = ClientBuilder.newClient().target(baseUri);
+        }
+        public PipelineClient(String baseUri,String clientId,String secret) {
+                this.target = ClientBuilder.newClient(new ClientConfig().register(new Authenticator(clientId,secret))).target(baseUri);
         }
         private <T> T get(String path,Class<T> result) {
                 
@@ -103,6 +124,54 @@ public class PipelineClient {
         }
         public Client client(String id) throws Exception{
                 return this.get(String.format("admin/clients/%s",id),Client.class);
+        }
+
+        private static class Authenticator implements ClientRequestFilter {
+                private String clientId;
+                private String secret;
+
+                /**
+                 * @param clientId
+                 * @param secret
+                 */
+                public Authenticator(String clientId, String secret) {
+                        this.clientId = clientId;
+                        this.secret = secret;
+                }
+
+                @Override
+                public void filter(ClientRequestContext ctxt) throws IOException {
+                        String timestamp=new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+                        .format(new Date());
+                        String nonce=new Integer(new Random().nextInt(1073741824)).toString();
+                        nonce=Strings.padStart(nonce,30,'0');
+                        
+                        UriBuilder builder=UriBuilder.fromUri(ctxt.getUri());
+                        builder.queryParam("authid",this.clientId);
+                        builder.queryParam("time",timestamp);
+                        builder.queryParam("nonce",nonce);
+                        //logger.info("secret "+secret);
+                        SecretKeySpec key = new SecretKeySpec((this.secret).getBytes("UTF-8"), "HmacSHA1");
+                        try {
+                                Mac mac = Mac.getInstance("HmacSHA1");
+                                mac.init(key);
+
+
+                                byte[] bytes = mac.doFinal(builder.clone().build()
+                                                .toString().getBytes("UTF-8"));
+
+                                builder.queryParam("sign",Base64.encodeBase64String(bytes));
+                                ctxt.setUri(builder.build());
+                        } catch (Exception e) {
+                                logger.warn(e.getMessage());
+                                throw new RuntimeException(e);
+                        }
+
+
+                        
+
+                }
+                
         }
 }
 
