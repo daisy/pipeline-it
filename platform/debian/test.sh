015444311b4aaaf3a1222413923302de21139e9e
Bert Frees <bertfrees@gmail.com>
2015-01-22 22:25:31 +0100

    Test for starting the pipeline daemon on Debian and running zedai-to-pef
    see https://github.com/daisy/pipeline-issues/issues/440
    
    Replaces the previous test.
    
    The pipeline image is now built from the Dockerfile in pipeline-assembly.
    
    Dockerfiles for nexus and reprepro were removed. They now live in their
    own repos at snaekobbi/nexus-service and snaekobbi/reprepro-service,
    along with their own tests. If one of these services is needed in this
    project they can be built from source or pulled from an image registry.
    
    PS: the reason vagrant is not used anymore is because I don't need a VM
    anymore in order to use docker. We can revert if someone needs it.

--- platform/debian/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-vagrant ssh -c "docker-enter reprepro /bin/update-repository.sh; docker run --link reprepro:reprepro daisy/debian bash -c \"apt-get update && apt-get install -y --force-yes daisy-pipeline2\""
